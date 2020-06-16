402c2f01804f273678c2b74296f6d4fde30990f6
Manish Khedekar <mankhedekar96@gmail.com>
2020-06-16 11:09:49 +0530

    Updates

---
+++ precache-manifest.9048ddecf4ba70750637ec68b8978410.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd773d28e7d2d25899a9d15351e557d",
+    "url": "/snakes-and-ladders/index.html"
+  },
+  {
+    "revision": "abdeff53a9ddc2988c84",
+    "url": "/snakes-and-ladders/static/css/main.a34e635c.chunk.css"
+  },
+  {
+    "revision": "d94386479ee518317c15",
+    "url": "/snakes-and-ladders/static/js/2.c52202f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snakes-and-ladders/static/js/2.c52202f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abdeff53a9ddc2988c84",
+    "url": "/snakes-and-ladders/static/js/main.6c1fbf9f.chunk.js"
+  },
+  {
+    "revision": "84a43e439b6656e2cd9a",
+    "url": "/snakes-and-ladders/static/js/runtime-main.2e130ab5.js"
+  },
+  {
+    "revision": "ea57322367527cd2d60235fe537acea1",
+    "url": "/snakes-and-ladders/static/media/location.ea573223.svg"
+  },
+  {
+    "revision": "0cf046b39d51deab7ac3b79cc4cf43e5",
+    "url": "/snakes-and-ladders/static/media/snakes-ladders-board.0cf046b3.png"
+  }
+]);
